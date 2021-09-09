5afceae616e671773f0a401e969ce7efdb654880
Pinheiro <taysa.pinheiro@capgemini.com>
2021-09-09 17:28:05 -0300

    novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,9 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { RouterModule } from '@angular/router';
 import {ROUTES} from './app.routes';
-import { OfertaComponent } from './oferta/oferta.component'
+import { OfertaComponent } from './oferta/oferta.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component'
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { OfertaComponent } from './oferta/oferta.component'
     RodapeComponent,
     DiversaoComponent,
     RestaurantesComponent,
-    OfertaComponent
+    OfertaComponent,
+    ComoUsarComponent,
+    OndeFicaComponent
   ],
   imports: [
     BrowserModule,
