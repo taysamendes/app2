7feb44f566dafdfafe7c00697a167ed2d03a2066
Pinheiro <taysa.pinheiro@capgemini.com>
2021-09-10 10:39:40 -0300

    novo bd, corrigindo url e descrição de como-usar

--- src/app/oferta/como-usar/como-usar.component.ts
+++ src/app/oferta/como-usar/como-usar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from 'src/app/ofertas.service';
 
 @Component({
   selector: 'app-como-usar',
   templateUrl: './como-usar.component.html',
-  styleUrls: ['./como-usar.component.css']
+  styleUrls: ['./como-usar.component.css'],
+  providers: [OfertasService]
 })
 export class ComoUsarComponent implements OnInit {
 
-  constructor() { }
+  public comoUsar: string = ''
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+    ) { }
 
   ngOnInit(): void {
+    this.ofertasService.comoUsarOfertaPorId(this.route.parent?.snapshot.params['id'])
+      .then((descricao: any)=>{
+        console.log(descricao)
+        this.comoUsar = descricao
+      })
   }
 
 }
