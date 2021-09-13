4fd39228865826e69fde2ab08f19f335fba36c71
Pinheiro <taysa.pinheiro@capgemini.com>
2021-09-13 10:38:00 -0300

    programação reativa usando subject, switchMap, debounceTime,of

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
+import { debounceTime, switchMap } from 'rxjs/operators';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 
@@ -12,19 +13,37 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas!: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this.ofertas = this.subjectPesquisa //retorno Oferta[]
+      .pipe(debounceTime(1000))
+      .pipe(switchMap((termo: string) => {
+        console.log('----- Requisição ----')
+        if(termo.trim()===''){
+          return of<Oferta[]>([]) // retornar array vazio caso o campo de pesquisa esteja sem termo
+        }
+        return this.ofertasService.pesquisaOfertas(termo)
+      }))
+
+      this.ofertas.subscribe((ofertas:Oferta[])=>{
+        console.log(ofertas)
+      })
   }
 
   public pesquisa(termoDaPesquisa: string): void {
-   this.ofertas =  this.ofertasService.pesquisaOfertas(termoDaPesquisa)
-   this.ofertas.subscribe(
-    (data: Oferta[]) => console.log(data),
-    (error: any) => console.log('Erro status: ',error.status)
-   )
-     
+    /*this.ofertas =  this.ofertasService.pesquisaOfertas(termoDaPesquisa)
+    this.ofertas.subscribe(
+     (data: Oferta[]) => console.log(data),
+     (error: any) => console.log('Erro status: ',error.status)
+    )*/
+
+    console.log('key up do caracter: ',termoDaPesquisa)
+
+    this.subjectPesquisa.next(termoDaPesquisa)
+
   }
 
 }
